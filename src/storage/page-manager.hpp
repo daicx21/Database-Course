ab9bfb6599beb19abcbeb4b3dab7a6a66ffb905c
daichenxin21 <daicx21@mails.tsinghua.edu.cn>
2023-05-07 10:36:36 +0800

    commit

--- src/storage/page-manager.hpp
+++ src/storage/page-manager.hpp
@@ -4,6 +4,7 @@
 #include "common/error.hpp"
 #include "common/logging.hpp"
 
+#include<iostream>
 #include <algorithm>
 #include <cassert>
 #include <cstdint>
@@ -18,35 +19,6 @@
 #include <vector>
 
 namespace wing {
-
-// Return the minimum element in [start, end) which >= "d".
-// Return "end" if all elements in [start, end) < "d".
-template <typename Addable, typename T, typename ThreeWayCompare>
-Addable LowerBoundAddable(Addable start, Addable end, T d, ThreeWayCompare comp) {
-  while (start != end) {
-    Addable mid = start + (end - start) / 2;
-    if (comp(mid, d) == std::weak_ordering::less) {
-      start = mid + 1;
-    } else {
-      end = mid;
-    }
-  }
-  return start;
-}
-// Return the minimum element in [start, end) which > "d".
-// Return "end" if all elements in [start, end) <= "d".
-template <typename Addable, typename T, typename ThreeWayCompare>
-Addable UpperBoundAddable(Addable start, Addable end, T d, ThreeWayCompare comp) {
-  while (start != end) {
-    Addable mid = start + (end - start) / 2;
-    if (comp(mid, d) == std::weak_ordering::greater) {
-      end = mid;
-    } else {
-      start = mid + 1;
-    }
-  }
-  return start;
-}
 
 class PageManager;
 
@@ -75,24 +47,14 @@ public:
     page.id_ = 0;
     return *this;
   }
-  // When destructing, the reference to the underlying page buffer will be
-  // dropped.
   ~Page();
   inline pgid_t ID() const { return id_; }
   inline const char *as_ptr() const { return page_; }
-  // You should mark the page as dirty if you modify it, so that the page will
-  // be flushed to disk when evicted.
-  inline void MarkDirty() { dirty_ = true; }
-  // Drop the reference to the underlying page buffer. Note that this does not
-  // free the page, which is the job of PageManager::Free
+  inline void MarkDirty() { dirty_=true; }
   inline void Drop();
 protected:
-  Page(pgid_t id, char *page, std::reference_wrapper<PageManager> pgm,
-      bool dirty)
-    : id_(id), page_(page), pgm_(pgm), dirty_(dirty) {}
-  inline pgoff_t Offset(void *addr) {
-    return (pgoff_t)((char *)addr - page_);
-  }
+  Page(pgid_t id,char *page,std::reference_wrapper<PageManager> pgm,bool dirty):id_(id),page_(page),pgm_(pgm),dirty_(dirty) {}
+  inline pgoff_t Offset(void *addr) { return (pgoff_t)((char *)addr-page_); }
   inline void __Drop();
   pgid_t id_;
   char *page_;
@@ -101,7 +63,6 @@ protected:
   friend class PageManager;
 };
 
-// The handle that references a page buffer whose format is PlainPage.
 class PlainPage : public Page {
 public:
   PlainPage(const PlainPage&) = delete;
@@ -150,176 +111,193 @@ private:
  * The parse of slots is performed in SlotKeyCompare and SlotCompare,
  * so we don't have to care about the content of slots in this class.
  */
-template <typename SlotKeyCompare, typename SlotCompare>
-class SortedPage : public Page {
+template <typename SlotKeyCompare,typename SlotCompare>
+class SortedPage:public Page
+{
 public:
-  SortedPage(Page&& page, const SlotKeyCompare& slot_key_comp,
-      const SlotCompare& slot_comp)
-    :	Page(std::move(page)),
-      slot_key_comp_(slot_key_comp),
-      slot_comp_(slot_comp) {}
-  SortedPage(const SortedPage&) = delete;
-  SortedPage& operator=(const SortedPage&) = delete;
-  SortedPage(SortedPage&& page)
-    :	Page(std::move(page)),
-      slot_key_comp_(page.slot_key_comp_),
-      slot_comp_(page.slot_comp_) {}
+  SortedPage(Page&& page, const SlotKeyCompare& slot_key_comp, const SlotCompare& slot_comp):Page(std::move(page)),slot_key_comp_(slot_key_comp),slot_comp_(slot_comp) {}
+  SortedPage(const SortedPage&)=delete;
+  SortedPage& operator=(const SortedPage&)=delete;
+  SortedPage(SortedPage&& page): Page(std::move(page)),slot_key_comp_(page.slot_key_comp_),slot_comp_(page.slot_comp_) {}
   SortedPage& operator=(SortedPage&& page) {
     Page::operator=(std::move(page));
     return *this;
   }
-  void Init(std::size_t special_size) {
+  inline void Init(std::size_t special_size) {
     MarkDirty();
-    slotid_t *num = (slotid_t *)page_;
-    *num = 0;
-    pgoff_t *special = (pgoff_t *)(num + 1);
-    *special = SIZE - special_size;
+    slotid_t *num=(slotid_t *)page_;*num=0;
+    pgoff_t *special=(pgoff_t *)(num+1);*special=SIZE-special_size;
   }
   inline slotid_t SlotNum() const { return *(const slotid_t *)page_; }
   inline slotid_t& SlotNumMut() {
     MarkDirty();
     return *(slotid_t *)page_;
   }
-  inline bool IsEmpty() const { return SlotNum() == 0; }
-  // Return the mutable pointer to the start of the slot.
+  inline bool IsEmpty() const { return (SlotNum()==0); }
+  inline const char *SlotRaw(slotid_t slot) const {
+    assert(slot<SlotNum());
+    return page_+Starts()[slot];
+  }
   inline char *SlotRawMut(slotid_t slot) {
     MarkDirty();
-    assert(slot < SlotNum());
-    return page_ + StartsMut()[slot];
+    assert(slot<SlotNum());
+    return page_+StartsMut()[slot];
   }
   inline std::string_view Slot(slotid_t slot) const {
-    assert(slot < SlotNum());
-    return std::string_view(SlotRaw(slot), SlotSize(slot));
+    assert(slot<SlotNum());
+    return std::string_view(SlotRaw(slot),SlotSize(slot));
   }
-  /* Read data from the special space.
-   *  start: start point's offset in special space.
-   *  len: the length in bytes to read.
-   */
-  inline std::string_view ReadSpecial(pgoff_t start, pgoff_t len) const {
-    return std::string_view(Special() + start, len);
-  }
-  /* Write data to the special space.
-   *  start: start point's offset in special space.
-   *  data: the data to write.
-   */
-  inline void WriteSpecial(pgoff_t start, std::string_view data) {
+  inline std::string_view ReadSpecial(pgoff_t start,pgoff_t len) const { return std::string_view(Special()+start,len); }
+  inline void WriteSpecial(pgoff_t start,std::string_view data) {
     MarkDirty();
-    memcpy(SpecialMut() + start, data.data(), data.size());
+    memcpy(SpecialMut()+start,data.data(),data.size());
   }
-  // Return whether we can insert the slot into the page without splitting.
-  inline bool IsInsertable(std::string_view slot) const {
-    return FreeSpace() >= slot.size() + sizeof(pgoff_t);
+  inline bool IsInsertable(std::string_view slot) const { return (FreeSpace()>=slot.size()+sizeof(pgoff_t)); }
+  inline bool IsReplacable(slotid_t slotid,std::string_view slot) const { return (FreeSpace()+SlotSize(slotid)>=slot.size()); }
+  slotid_t gao1(slotid_t l, slotid_t r,std::string_view d) const {
+    while (l!=r)
+    {
+      slotid_t mid=(l+r)/2;
+      if (slot_key_comp_(Slot(mid),d)==std::weak_ordering::less) l=mid+1;
+      else r=mid;
+    }
+    return l;
   }
-
-  // You should add some interfaces here to facilitate BPlusTree.
-  // Here are some example interfaces that you may adopt.
-
-  // Find the slot with the minimum key s.t. key >= "key" in argument.
-  // If this slot doesn't exist, return SlotNum().
-  slotid_t LowerBound(std::string_view key) const {
-    DB_ERR("Not implemented yet!");
+  slotid_t gao2(slotid_t l,slotid_t r,std::string_view d) const {
+    while (l!=r)
+    {
+      slotid_t mid=(l+r)/2;
+      if (slot_key_comp_(Slot(mid),d)==std::weak_ordering::greater) r=mid;
+      else l=mid+1;
+    }
+    return l;
   }
-  // Find the slot with the minimum key s.t. key > "key" in argument
-  // If this slot doesn't exist, return SlotNum().
-  slotid_t UpperBound(std::string_view key) const {
-    DB_ERR("Not implemented yet!");
-  }
-  // Find the key and return the slot ID.
-  // If this key doesn't exist, return SlotNum().
+  slotid_t LowerBound(std::string_view key) const { return gao1(0,SlotNum(),key); }
+  slotid_t UpperBound(std::string_view key) const { return gao2(0,SlotNum(),key); }
   slotid_t Find(std::string_view key) const {
-    DB_ERR("Not implemented yet!");
+    slotid_t n=SlotNum(),h1=gao1(0,n,key);
+    if (h1<n&&slot_key_comp_(Slot(h1),key)==std::weak_ordering::equivalent) return h1;
+    return n;
   }
-  // Find the key and return the slot.
-  // If the key is not found, return std::nullopt.
+  slotid_t Find1(std::string_view key) const {
+    slotid_t n=SlotNum(),h1=gao1(0,n,key);
+    if (h1<n&&slot_key_comp_(Slot(h1),key)==std::weak_ordering::equivalent) return n+1;
+    return h1;
+  }
   std::optional<std::string_view> FindSlot(std::string_view key) const {
-    DB_ERR("Not implemented yet!");
+    slotid_t n=SlotNum();
+    slotid_t h1=gao1(0,n,key);
+    if (h1<n&&slot_key_comp_(Slot(h1),key)==std::weak_ordering::equivalent) return Slot(h1);
+    return std::nullopt;
   }
-  /* Append the slot as the last slot of this page without checking whether the
-   * page will overflow.
-   */
   void AppendSlotUnchecked(std::string_view slot) {
     MarkDirty();
-    pgoff_t tail_offset = Ends()[SlotNum()];
-    pgoff_t start = tail_offset - slot.size();
-    memcpy(page_ + start, slot.data(), slot.size());
-    StartsMut()[SlotNum()] = start;
-    SlotNumMut() += 1;
+    pgoff_t tail_offset=Ends()[SlotNum()];
+    pgoff_t start=tail_offset-slot.size();
+    memcpy(page_+start,slot.data(),slot.size());
+    StartsMut()[SlotNum()]=start;
+    SlotNumMut()+=1;
   }
-  /* Insert the slot before the given slot. e.g., if the given slot ID is 4,
-   *  then the new slot will be inserted before the original slot 4.
-   *  After insertion, the new slot will be slot 4, and the original slot 4
-   *  will be the slot 5, and so on.
-   * Return succeed or not.
-   */
-  inline bool InsertBeforeSlot(slotid_t slotid, std::string_view slot) {
-    DB_ERR("Not implemented yet!");
+  inline bool InsertBeforeSlot(slotid_t slotid,std::string_view slot)
+  {
+    if (!IsInsertable(slot)) return false;
+    MarkDirty();
+    slotid_t n=SlotNum();
+    pgoff_t h1=Starts()[n-1],h2=Ends()[slotid];
+    memmove(page_+h1-slot.size(),page_+h1,h2-h1);
+    for (slotid_t j=1;j<=n-slotid;j++)
+    {
+      slotid_t i=n-j;
+      StartsMut()[i+1]=Starts()[i]-slot.size();
+    }
+    h1=h2-slot.size();
+    memcpy(page_+h1,slot.data(),h2-h1);
+    StartsMut()[slotid]=h1;SlotNumMut()+=1;
+    return true;
   }
-  /* Logically equivalent to inserting the slot before the given slot, and then
-   *  split the right half of the overflowed page into the empty page "right".
-   * right: an empty page to accommodate the right half of the overflowed page.
-   * slot: the slot to insert.
-   * slotid: the slot that the new slot will be inserted before.
-   * Return value:
-   *	true: Succeed.
-   *	false: If insert the slot, there will be no way to split the resulting
-   *		page into two pages without overflow.
-   */
-  bool SplitInsert(SortedPage<SlotKeyCompare, SlotCompare>& right,
-      std::string_view slot, slotid_t slotid) {
-    DB_ERR("Not implemented yet!");
+  bool SplitInsert(SortedPage<SlotKeyCompare,SlotCompare>& right,slotid_t slotid,std::string_view slot) {
+    MarkDirty();
+    if (IsInsertable(slot)) return InsertBeforeSlot(slotid,slot);
+    slotid_t n=SlotNum();
+    if (Ends()[slotid]-sizeof(slotid_t)-sizeof(pgoff_t)-sizeof(pgoff_t)*slotid>=sizeof(pgoff_t)+slot.size())
+    {
+      for (slotid_t i=slotid;i<n;i++) right.AppendSlotUnchecked(Slot(i));
+      SlotNumMut()=slotid+1;
+      StartsMut()[slotid]=Ends()[slotid]-slot.size();
+      memcpy(page_+Starts()[slotid],slot.data(),slot.size());
+    }
+    else
+    {
+      right.AppendSlotUnchecked(slot);
+      for (slotid_t i=slotid;i<n;i++) right.AppendSlotUnchecked(Slot(i));
+      SlotNumMut()=slotid;
+    }
+    return true;
   }
-  /* Logically equivalent to replacing the given slot, and then split the right
-   *  half of the overflowed page into the empty page "right".
-   * slot: the slot to insert.
-   * slotid: the slot that will be replaced by the new slot.
-   * Return value:
-   *	true: Succeed.
-   *	false: If replace the slot, there will be no way to split the resulting
-   *		page into two pages without overflow.
-   */
-  bool SplitReplace(SortedPage<SlotKeyCompare, SlotCompare>& right,
-      std::string_view slot, slotid_t slotid) {
-    DB_ERR("Not implemented yet!");
+  bool Replace(slotid_t slotid,std::string_view slot)
+  {
+    MarkDirty();
+    memcpy(page_+Starts()[slotid],slot.data(),slot.size());
+    return true;
+  }
+  bool ReplaceSlot(slotid_t slotid,std::string_view slot)
+  {
+    slotid_t n=SlotNum();
+    pgoff_t h1=Starts()[n-1],h2=Starts()[slotid];
+    signed_pgoff_t hh=(Ends()[slotid]-h2)-slot.size();
+    memmove(page_+h1+hh,page_+h1,h2-h1);
+    for (slotid_t i=slotid;i<n;i++) StartsMut()[i]+=hh;
+    memcpy(page_+Starts()[slotid],slot.data(),slot.size());
+    return true;
+  }
+  bool SplitReplace(SortedPage<SlotKeyCompare,SlotCompare>& right,slotid_t slotid,std::string_view slot) {
+    MarkDirty();
+    if (IsReplacable(slotid,slot)) return ReplaceSlot(slotid,slot);
+    slotid_t n=SlotNum();
+    if (Ends()[slotid]-sizeof(slotid_t)-sizeof(pgoff_t)-sizeof(pgoff_t)*slotid>=sizeof(pgoff_t)+slot.size())
+    {
+      for (slotid_t i=slotid+1;i<n;i++) right.AppendSlotUnchecked(Slot(i));
+      SlotNumMut()=slotid+1;
+      StartsMut()[slotid]=Ends()[slotid]-slot.size();
+      memcpy(page_+Starts()[slotid],slot.data(),slot.size());
+    }
+    else
+    {
+      right.AppendSlotUnchecked(slot);
+      for (slotid_t i=slotid+1;i<n;i++) right.AppendSlotUnchecked(Slot(i));
+      SlotNumMut()=slotid;
+    }
+    return true;
+  }
+  void DeleteSlot(slotid_t slotid) {
+    MarkDirty();
+    slotid_t n=SlotNum();
+    pgoff_t h1=Starts()[n-1],h2=Starts()[slotid],hh=Ends()[slotid]-h2;
+    memmove(page_+h1+hh,page_+h1,h2-h1);
+    for (slotid_t i=slotid;i<n-1;i++) StartsMut()[i]=Starts()[i+1]+hh;
+    SlotNumMut()-=1;
   }
 
-  // Delete the slot specified by slot ID.
-  void DeleteSlot(slotid_t slot_id) {
-    DB_ERR("Not implemented yet!");
-  }
-  // Delete the slot specified by the key.
-  // Return whether the deletion is successful or not.
-  bool DeleteSlotByKey(std::string_view key) {
-    DB_ERR("Not implemented yet!");
-  }
 private:
-  // Some helper classes/functions that you may adopt.
   class ComparePageOffKey {
   public:
-    // start: the start point offset of the slot to be compared.
-    std::weak_ordering operator()(
-      const pgoff_t *start, std::string_view key
-    ) const {
-      size_t len = *(start - 1) - *start;
-      std::string_view slot(page_ + *start, len);
-      return slot_key_comp_(slot, key);
+    std::weak_ordering operator()(const pgoff_t *start,std::string_view key) const {
+      size_t len=*(start-1)-*start;
+      std::string_view slot(page_+*start,len);
+      return slot_key_comp_(slot,key);
     }
   private:
-    ComparePageOffKey(const char *page, SlotKeyCompare slot_key_comp)
-      :	page_(page), slot_key_comp_(slot_key_comp) {}
+    ComparePageOffKey(const char *page,SlotKeyCompare slot_key_comp):page_(page),slot_key_comp_(slot_key_comp) {}
     const char *page_;
     SlotKeyCompare slot_key_comp_;
     friend class SortedPage;
   };
   class ComparePageOffSlot {
   public:
-    // start: the start point offset of the slot to be compared.
-    // slot: the content of the slot.
-    std::weak_ordering operator()(
-      const pgoff_t *start, std::string_view slot
-    ) const {
-      size_t len = *(start - 1) - *start;
-      std::string_view cur_slot(page_ + *start, len);
-      return slot_comp_(cur_slot, slot);
+    std::weak_ordering operator()(const pgoff_t *start,std::string_view slot) const {
+      size_t len=*(start-1)-*start;
+      std::string_view cur_slot(page_+*start,len);
+      return slot_comp_(cur_slot,slot);
     }
   private:
     ComparePageOffSlot(const char *page, SlotCompare slot_comp)
@@ -328,89 +306,39 @@ private:
     SlotCompare slot_comp_;
     friend class SortedPage;
   };
-  // Raw pointer to the start of the special space.
-  inline const char *Special() const { return page_ + Ends()[0]; }
-  // Mutable raw pointer to the start of the special space.
+  inline const char *Special() const { return page_+Ends()[0]; }
   inline char *SpecialMut() {
     MarkDirty();
     return page_ + EndsMut()[0];
   }
-  /* Return an array containing the offsets of slots' ends.
-   * Note that the offset of the special space's start point is equivalent to
-   * the offset of slot 0's end, and the offset of slot i's start point is
-   * equivalent to the offset of slot (i+1)'s end.
-   */
   inline const pgoff_t *Ends() const {
-    const pgoff_t *special = (const pgoff_t *)((const slotid_t *)page_ + 1);
+    const pgoff_t *special=(const pgoff_t*)((const slotid_t *)page_+1);
     return special;
   }
-  // Return a mutable array containing the offsets of slots' ends.
   inline pgoff_t *EndsMut() {
     MarkDirty();
     slotid_t *num = (slotid_t *)page_;
     pgoff_t *special = (pgoff_t *)(num + 1);
     return special;
   }
-  /* Return an array containing the offsets of slots' start points.
-   * Note that the offset of slot (i+1)'s end is equivalent to the offset
-   * of slot i's start point.
-   */
-  inline const pgoff_t *Starts() const {
-    return Ends() + 1;
-  }
-  // Return a mutable array containing the offsets of slots' start points.
+  inline const pgoff_t *Starts() const { return Ends()+1; }
   inline pgoff_t *StartsMut() {
     MarkDirty();
-    return EndsMut() + 1;
+    return EndsMut()+1;
   }
-  // Return the pointer to the start of the slot.
-  inline const char *SlotRaw(slotid_t slot) const {
-    assert(slot < SlotNum());
-    return page_ + Starts()[slot];
-  }
-  // The difference between the slot's end offset and start offset is its size.
   inline pgoff_t SlotSize(slotid_t slot) const {
-    const pgoff_t *ends = Ends();
-    const pgoff_t *starts = ends + 1;
-    return ends[slot] - starts[slot];
+    const pgoff_t *ends=Ends();
+    const pgoff_t *starts=ends+1;
+    return ends[slot]-starts[slot];
   }
-  // Return the space occupied by the given slot, including the size of the
-  // slot itself and the size of its start point's offset.
-  inline pgoff_t SlotSpace(slotid_t slot) const {
-    return SlotSize(slot) + sizeof(pgoff_t);
-  }
-  // Return the size of slots whose ID are in range [start, end).
-  inline pgoff_t SlotsSize(slotid_t start, slotid_t end) const {
-    return Ends()[start] - Ends()[end];
-  }
-  // Return the space occupied by slots whose ID are in range [start, end).
-  // Including the size of the slots themselves and the size of their start
-  // points' offsets.
-  inline pgoff_t SlotsSpace(slotid_t start, slotid_t end) const {
-    return SlotsSize(start, end) + (end - start) * sizeof(pgoff_t);
-  }
+  inline pgoff_t SlotSpace(slotid_t slot) const { return SlotSize(slot)+sizeof(pgoff_t); }
+  inline pgoff_t SlotsSize(slotid_t start,slotid_t end) const { return Ends()[start]-Ends()[end]; }
+  inline pgoff_t SlotsSpace(slotid_t start,slotid_t end) const { return SlotsSize(start,end)+(end-start)*sizeof(pgoff_t); }
   // Return the size of free space in this page.
   inline pgoff_t FreeSpace() const {
-    slotid_t num = SlotNum();
-    const pgoff_t *ends = Ends();
-    // ends[num] is start_{N-1}
-    return ends[num] - sizeof(slotid_t) - sizeof(pgoff_t) -
-      sizeof(pgoff_t) * num;
-  }
-  /* Return whether we can replace the given slot with the new one without
-   * splitting.
-   * slot_id: the slot ID of the slot to be replaced.
-   * slot: the content of the new slot.
-   */
-  inline bool IsReplacable(slotid_t slot_id, std::string_view slot) const {
-    return FreeSpace() + SlotSize(slot_id) >= slot.size();
-  }
-  /* Return whether the slots in "src" whose ID are in range [start, end) can
-   * be appended to this page without splitting.
-   */
-  inline bool IsAppendable(SortedPage<SlotKeyCompare, SlotCompare>& src,
-      slotid_t start, slotid_t end) const {
-    return FreeSpace() >= src.SlotsSpace(start, end);
+    slotid_t num=SlotNum();
+    const pgoff_t *ends=Ends();
+    return ends[num]-sizeof(slotid_t)-sizeof(pgoff_t)-sizeof(pgoff_t)*num;
   }
 
   SlotKeyCompare slot_key_comp_;
